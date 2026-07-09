7043efd2a40995a5c4b01e18e2629df029377ce8
Rishu Patel <rishupatel7756@gmail.com>
2026-07-09 12:45:54 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Rishu Patel");
+        System.out.println("Age: 20");
+        System.out.println("College : IIMT College Of Engineering");
+    }
+}
